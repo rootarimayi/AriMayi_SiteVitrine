c663833526b8ec579b3a68ff482ed620627d8ff8
Aziza H <mess.aziza@gmail.com>
2024-11-08 14:59:21 +0100

    add Footer component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,10 @@
+
+export default function Footer() {
+  return (
+    <footer className='bg-black py-2 text-center'>
+      <span className="text-white  text-[16px] md:text-[20px] font-Opensans ">
+        © 2024 AriMayi.Tous droits réservés.
+      </span>
+    </footer>
+  );
+}
