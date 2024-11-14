f19fa857c08d22078f91e384a44880e816efa59c
Aziza H <mess.aziza@gmail.com>
2024-11-14 14:33:47 +0100

    delete one card of homePage

--- app/page.js
+++ app/page.js
@@ -28,7 +28,7 @@ export default function Home() {
           <Card image={cardImg} titre="Je cherche à gagner de l'expérience" content="Jeune diplômé sans emploi ?
           Rejoignez notre organisme pour une formation ciblée qui vous démarque. Développez des compétences recherchées par les recruteurs grâce à nos projets concrets et décrochez enfin le poste que vous méritez" className="" />
           <Card image={cardImg2} titre="Je recherche un nouveau métier dans l'IT" content="Rejoignez notre organisme et transformez votre carrière dans l’IT ! Profitez d’une formation complète et de notre accompagnement vers des projets concrets pour réussir votre reconversion dans un secteur en plein essor" className="" />
-          <Card image={cardImg3} titre="Je recherche un contrat d'apprentissage" content="Boostez votre parcours avec un contrat d'apprentissage intégrant la POEI (Préparation Opérationnelle à l’Emploi Individuelle) : grâce à notre référentiel de partenaires, accédez à des projets enrichissants qui conjuguent formation et expérience concrète en entreprise" className="" />
+
         </div>
       </div>
     </>
