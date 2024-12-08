22f19a8d7a2f75257c965aa426f85208d388417c
nouhaila echchnaoui <nouhaila.chan2001@gmail.com>
2024-12-08 08:33:15 +0100

    update faq

--- app/components/Collapse.jsx
+++ app/components/Collapse.jsx
@@ -7,15 +7,29 @@ export default function Collapse({ title, content }) {
 	const [isOpen, setIsOpen] = useState(false);
 
 	return (
-		<div className="dropdown w-[70%] m-2 ">
-			<div className="dropdown-title bg-[#F68A67] flex justify-between items-center p-3 rounded-t-md ">
-				<p>{title}</p>
-				<Image src={downIcon} alt='down-icon'  style={{ 
-          transform: isOpen ? 'rotate(180deg)' : null}} onClick={() => setIsOpen((curent) => !curent)} className="down-icon"
+    <div className="dropdown w-[70%] m-2">
+      <div
+        className="dropdown-title bg-[#FEF9F5] flex justify-between items-center p-8 rounded-t-md cursor-pointer transition-all duration-300 ease-in-out"
+        onClick={() => setIsOpen((current) => !current)}
+      >
+        <p className="text-gray-800 font-semibold">{title}</p>
+        <Image
+          src={downIcon}
+          alt="down-icon"
+          style={{
+            transform: isOpen ? "rotate(180deg)" : null,
+          }}
+          className="down-icon transition-transform duration-300 ease-in-out"
         />
-			</div>
+      </div>
 
-			{isOpen && <div className='dropdown-content bg-[#FFECDF] bg-opacity-50 p-4 rounded-b-md'>{content}</div>}
-		</div>
-	);
+      <div
+        className={`dropdown-content bg-[#FEF9F5] bg-opacity-50 p-8 rounded-b-md transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        {content}
+      </div>
+    </div>
+  );
 }
