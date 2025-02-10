b3c0fdea6ae7f113462a69563deb93297feaf4b7
MDSiyam07 <59643688+MDSiyam07@users.noreply.github.com>
2025-02-10 15:18:05 +0100

    [#] Delete console log in component

--- app/components/Contact/Coordonnees.jsx
+++ app/components/Contact/Coordonnees.jsx
@@ -55,8 +55,6 @@ export default function Coordonnees() {
                 },
                 body: JSON.stringify(formData),
             });
-            console.log("Headers:", request.headers.get("content-type"));
-
 
             if (!response.ok) {
             const errorData = await response.json();
