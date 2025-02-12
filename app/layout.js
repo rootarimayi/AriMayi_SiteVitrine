6b5703581340ca903cf11b80f2ab884f50ecc6e5
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2025-02-12 18:34:33 +0000

    Add devContainer for dev and Google Analytics

--- app/layout.js
+++ app/layout.js
@@ -7,6 +7,7 @@ import "./globals.css";
 import { Open_Sans, Montserrat } from 'next/font/google'
 import DynamicLayout from "./components/Utils/DynamicLayout";
 import { Toaster } from 'react-hot-toast';
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const openSans = Open_Sans({
   subsets: ['latin'],
@@ -21,57 +22,58 @@ const montserrat = Montserrat({
 })
 
 export const metadata = {
+  title: "AriMayi",
+  description: "L'expérience concrète par la formation",
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme: light)',
+        url: '/favicon.png',
+      },
+      {
+        media: '(prefers-color-scheme: dark)',
+        url: '/favicon.png',
+      },
+    ],
+    apple: [
+      {
+        url: '/apple-touch-icon.png',
+      },
+    ],
+  },
+  other: {
+    canonical: "https://www.arimayi.io",
+  },
+  keywords: ["formation", "expérience", "éducation", "AriMayi"],
+  openGraph: {
     title: "AriMayi",
     description: "L'expérience concrète par la formation",
-    icons: {
-      icon: [
-        {
-          media: '(prefers-color-scheme: light)',
-          url: '/favicon.png',
-        },
-        {
-          media: '(prefers-color-scheme: dark)',
-          url: '/favicon.png',
-        },
-      ],
-      apple: [
-        {
-          url: '/apple-touch-icon.png',
-        },
-      ],
-    },
-    other: {
-      canonical: "https://www.arimayi.io",
-    },
-    keywords: ["formation", "expérience", "éducation", "AriMayi"],
-    openGraph: {
-      title: "AriMayi",
-      description: "L'expérience concrète par la formation",
-      url: "https://www.arimayi.io",
-      siteName: "AriMayi",
-      images: [
-        {
-          url: "/og-cover-image.png",
-          width: 1200,
-          height: 630,
-          alt: "AriMayi - L'expérience concrète par la formation",
-        },
-      ],
-      locale: "fr_FR",
-      type: "website",
-    },
+    url: "https://www.arimayi.io",
+    siteName: "AriMayi",
+    images: [
+      {
+        url: "https://arimayi.fr/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Flogo-arimayi-slogan.d8f4a610.png&w=384&q=75",
+        width: 1200,
+        height: 630,
+        alt: "AriMayi - L'expérience concrète par la formation",
+      },
+    ],
+    locale: "fr_FR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
-    return (
-      <html lang="fr">
-        <body className="antialiased">
-            <Toaster position="top-right" reverseOrder={false} />
-          <DynamicLayout>
-            {children}
-            </DynamicLayout>
-          {/* <Header />{children}< Footer/> */}
-        </body>
-      </html>
-    );
+  return (
+    <html lang="fr">
+      <body className="antialiased">
+        <GoogleAnalytics gaId="G-FKC9P276KR" />
+        <Toaster position="top-right" reverseOrder={false} />
+        <DynamicLayout>
+          {children}
+        </DynamicLayout>
+        {/* <Header />{children}< Footer/> */}
+      </body>
+    </html>
+  );
 }
